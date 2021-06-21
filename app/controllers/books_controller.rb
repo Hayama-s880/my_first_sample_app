aafcf815dabd93d57c23f0a1f59a4a766185aeb5
菅原隼真 <hayama.s@sugawarashunshinnoMacBook-Pro.local>
2021-06-21 16:05:21 +0900

    create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(books_params)
+    @book.save
+    redirect_to controller: :books, action: :index
+  end
+
+  private
+  def books_params
+    params.require(:book).permit(:title,:author,:pages)
+  end
 end
