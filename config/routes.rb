c3af3d93baeba6eb23114be1926c09bd349e6b94
菅原隼真 <hayama.s@sugawarashunshinnoMacBook-Pro.local>
2021-06-22 16:16:45 +0900

    books_edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'books#index'
   post '/books/delete/:id' => 'books#delete'
+  get '/books/edit/:id' => 'books#edit'
   
     #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :books, only: [:new,:create]
+  resources :books, only: [:new,:create,:update]
   
   
 end
